97409354ca3735d595ca667787523d27d9555ba3
Mohammad <ackrequest@gmail.com>
2026-02-21 23:50:23 +0330

    fix(fish): revert keybindings to fish 3

---
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,12 @@
+function fish_user_key_bindings
+    bind alt-left       prevd-or-backward-word
+    bind alt-right      nextd-or-forward-word
+    bind alt-backspace  backward-kill-word
+    bind alt-delete     kill-word
+
+    bind ctrl-left      backward-token
+    bind ctrl-right     forward-token
+    bind ctrl-backspace backward-kill-token
+    bind ctrl-delete    kill-token
+    bind ctrl-alt-h     backward-kill-word
+end
